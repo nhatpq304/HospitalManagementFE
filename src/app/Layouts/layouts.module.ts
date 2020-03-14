3be73138500ff82975116dc81557057189aeb398
Happeace304 <happeace304@gmail.com>
2020-03-14 18:52:42 +0700

    0314 header template

--- src/app/Layouts/layouts.module.ts
+++ src/app/Layouts/layouts.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [NavigationBarComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [NavigationBarComponent]
+  declarations: [NavigationBarComponent, SideBarComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent]
 })
-export class LayoutsModule { }
+export class LayoutsModule {}
