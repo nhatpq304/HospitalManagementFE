56b373823c749df493b8b48de1fe42707cbfa643
Happeace304 <happeace304@gmail.com>
2020-04-11 10:34:01 +0700

    0411 add button

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { UserDashboardComponent } from "../Users/dashboard/dashboard.component";
 import { MainComponent } from "./main.component";
+import { UserEditComponent } from "../Users/edit/edit.component";
 
 const routes: Routes = [
   {
     path: "default",
     component: MainComponent,
-    children: [{ path: "users", component: UserDashboardComponent }]
+    children: [
+      { path: "users", component: UserDashboardComponent },
+      { path: "users/add", component: UserEditComponent },
+      { path: "users/:id/edit", component: UserEditComponent }
+    ]
   }
 ];
 
