2301d3875428753e03fa16ad22d2604ab656ee25
Happeace304 <happeace304@gmail.com>
2020-04-11 21:49:16 +0700

    0411 on select data change, submit validate

--- src/app/Users/edit/edit.component.ts
+++ src/app/Users/edit/edit.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import formConfig from "./formConfig";
-import * as moment from "moment";
+import formUtil from "src/util/form.util";
+
 @Component({
   selector: "user-edit",
   templateUrl: "./edit.component.html",
@@ -11,24 +12,30 @@ import * as moment from "moment";
 export class UserEditComponent implements OnInit {
   resource;
   formConfig = formConfig;
+  hasLoginData: boolean;
   state = this.router.url === "/default/users/add" ? "ADD" : "EDIT";
+  userForm: FormGroup;
 
-  userForm = new FormGroup({
-    name: new FormControl("", [Validators.required]),
-    gender: new FormControl("", [Validators.required]),
-    idCard: new FormControl("", [Validators.pattern("^[0-9]*$")]),
-    medicalCard: new FormControl("", [Validators.pattern("^[0-9]*$")]),
-    address: new FormControl("", []),
-    phone: new FormControl("", [Validators.pattern("^[0-9]*$")]),
-    birthday: new FormControl("", []),
-    department: new FormControl("", []),
-    email: new FormControl("", [Validators.email]),
-    password: new FormControl("", [Validators.minLength(6)])
-  });
   constructor(public router: Router) {}
 
   ngOnInit(): void {
+    this.initForm();
     this.initResource();
+  }
+
+  initForm() {
+    this.userForm = new FormGroup({
+      name: new FormControl("", [Validators.required]),
+      gender: new FormControl("", [Validators.required]),
+      idCard: new FormControl("", [Validators.pattern("^[0-9]*$")]),
+      medicalCard: new FormControl("", [Validators.pattern("^[0-9]*$")]),
+      address: new FormControl("", []),
+      phone: new FormControl("", [Validators.pattern("^[0-9]*$")]),
+      birthday: new FormControl("", []),
+      department: new FormControl("", []),
+      email: new FormControl("", [Validators.email]),
+      password: new FormControl("", [Validators.minLength(6)])
+    });
   }
 
   initResource() {
@@ -39,7 +46,19 @@ export class UserEditComponent implements OnInit {
     };
   }
 
-  onSubmitClick() {}
+  onLoginAccordionClick() {
+    // this runs before 'show' rendered
+    this.hasLoginData = !$("#loginAccordionCollapse").hasClass("show");
+  }
+
+  onSubmitClick() {
+    formUtil.validateAllFormFields(
+      this.userForm,
+      this.hasLoginData ? null : ["email", "password"]
+    );
+    if (this.userForm.valid) {
+    }
+  }
 
   onDataChange(param) {
     this.userForm.controls[param.controlName].setValue(param.data);
