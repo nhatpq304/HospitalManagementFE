beee9d4daf2a404c80e36bc17cff178b0c5e23ef
Happeace304 <happeace304@gmail.com>
2020-04-11 16:57:46 +0700

    0411 text input, date picker

---
+++ src/app/Controls/datepicker/datepicker.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
+import * as moment from "moment";
+import { FormGroup } from "@angular/forms";
+@Component({
+  selector: "datepicker",
+  templateUrl: "./datepicker.component.html",
+  styleUrls: ["./datepicker.component.scss"]
+})
+export class DatepickerComponent implements OnInit, OnChanges {
+  @Input() config: any;
+  @Input() parentForm: FormGroup;
+  @Output() onDataChange = new EventEmitter();
+
+  constructor() {}
+  ngOnChanges() {}
+  ngOnInit(): void {
+    this.initDatePick();
+  }
+
+  private initDatePick() {
+    let self = this;
+    ($('input[name="date"]') as any).daterangepicker(
+      {
+        singleDatePicker: true,
+        showDropdowns: this.config?.showDropdowns || true,
+        minYear: this.config?.minYear || 1901,
+        maxYear: this.config?.maxYear || parseInt(moment().format("YYYY")),
+        locale: {
+          format: "DD/MM/YYYY"
+        }
+      },
+      function(start, end, label) {
+        self.onDataChanged(start.format("DD/MM/YYYY"));
+      }
+    );
+  }
+
+  private onDataChanged(param) {
+    this.onDataChange.emit({
+      data: param,
+      controlName: this.config.controlName
+    });
+  }
+}
--- src/app/Users/users.module.ts
+++ src/app/Users/users.module.ts
@@ -4,9 +4,20 @@ import { UserDashboardComponent } from "./dashboard/dashboard.component";
 import { ControlsModule } from "../Controls/controls.module";
 import { UserEditComponent } from "./edit/edit.component";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
 
 @NgModule({
   declarations: [UserDashboardComponent, UserEditComponent],
-  imports: [CommonModule, ControlsModule, RouterModule]
+  imports: [
+    CommonModule,
+    ControlsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MDBBootstrapModule,
+    BrowserModule,
+  ]
 })
 export class UsersModule {}
