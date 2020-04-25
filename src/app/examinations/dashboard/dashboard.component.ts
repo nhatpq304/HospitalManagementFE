ac5699626ac832c17147265637200f20bc970d7a
Happeace304 <happeace304@gmail.com>
2020-04-25 13:13:25 +0700

    0425 examination dashboard

--- src/app/examinations/dashboard/dashboard.component.ts
+++ src/app/examinations/dashboard/dashboard.component.ts
@@ -6,7 +6,45 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./dashboard.component.scss"],
 })
 export class ExaminationDashboardComponent implements OnInit {
+  resource;
+  datatableConfig;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initResource();
+    this.initGridConfig();
+    this.loadData();
+  }
+
+  private initResource() {
+    this.resource = {
+      stateTitle: "Quản lý kết quả khám",
+      addButton: {
+        title: "Thêm kết quả khám",
+        routerLink: "../examinations/add",
+      },
+      editRouterLink: "default/examinations/{id}/edit",
+    };
+  }
+
+  private loadData() {}
+
+  private initGridConfig() {
+    this.datatableConfig = {
+      id: "resultDatatableId",
+      columns: [
+        // { data: "email", title: "Email", type: "string" },
+        // { data: "name", title: "Tên", type: "string" },
+        // { data: "address", title: "Địa chỉ", type: "string" },
+        // { data: "birthday", title: "Ngày sinh", type: "Date" },
+        // { data: "id_card_number", title: "CMND", type: "string" },
+        // { data: "medical_card_number", title: "Số BHYT", type: "string" },
+        // {
+        //   data: "id",
+        //   title: "Sửa",
+        // },
+      ],
+      drawCallback: () => {},
+    };
+  }
 }
